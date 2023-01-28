cc336aa510230ab64335956878b0cbff0ef18176
Arpita Bhattacharjee <arpibhatt77@gmail.com>
2023-01-29 00:19:56 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -549,8 +549,8 @@ if(button):
                 fin= plot(dic,lat,lng)
                 #msg1= predict(age,mood)
                 st.write(fin)
-                if st.button("Clear All"):
-                     st.experimental_memo.clear()
+             #   if st.button("Clear All"):
+              #       st.experimental_memo.clear()
             #st.markdown(f'<p style="color:black;font-weight: bold;font-size:18px;">Here’s what we suggest: {}</p>', unsafe_allow_html=True)
             except:
                 msg2='Required Data are missing, Please key in all the data.'
