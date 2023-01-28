bf5cffa404bd23292339f0f1a962b44d89730120
Arpita Bhattacharjee <71565732+Arpi33@users.noreply.github.com>
2023-01-29 00:00:42 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -544,14 +544,14 @@ button = st.button("Recommend")
     
 if(button):
         if(len(dic)>0):
-            try:
+            #try:
                 lat,lng=gigi(result)
                 fin= plot(dic,lat,lng)
                 st.write(fin)
                 if st.button("Clear All"):
                      st.experimental_memo.clear()
             #st.markdown(f'<p style="color:black;font-weight: bold;font-size:18px;">Here’s what we suggest: {}</p>', unsafe_allow_html=True)
-            except:
-                msg2='Required Data are missing, Please key in all the data.'
-                st.markdown(f'<p style="color:red;font-weight: bold;font-size:18px; border-radius:2%;">{msg2}</p>', unsafe_allow_html=True)
+           # except:
+            #    msg2='Required Data are missing, Please key in all the data.'
+             #   st.markdown(f'<p style="color:red;font-weight: bold;font-size:18px; border-radius:2%;">{msg2}</p>', unsafe_allow_html=True)
 
